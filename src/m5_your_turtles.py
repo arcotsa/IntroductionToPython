36bac079efc3ec39ff2b3f6b1e3ebb24bb4dc457
arcots <arcots@rose-hulman.edu>
2017-11-27 10:57:51 -0500

    Completed m5

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Satya Arcot.
 """
 ########################################################################
-# TODO: 1.
+# DONEs: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
-
+import rosegraphics as rg
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,32 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+window = rg.TurtleWindow()
+
+green_turtle = rg.SimpleTurtle('turtle')
+green_turtle.pen = rg.Pen('green', 15)
+green_turtle.speed = 12
+radius = 150
+for k in range(30):
+    green_turtle.draw_circle(radius)
+
+    green_turtle.pen_up()
+    green_turtle.right(90)
+    green_turtle.forward(7)
+    green_turtle.pen_down()
+
+    radius = radius - 10
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red', 7)
+red_turtle.speed = 12
+size = 200
+for k in range(15):
+    red_turtle.draw_square(size)
+
+    red_turtle.pen_up()
+    red_turtle.left(90)
+    red_turtle.forward(10)
+    red_turtle.pen_down()
+
+    size = size - 20
